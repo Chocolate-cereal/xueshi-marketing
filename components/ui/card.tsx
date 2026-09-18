6c6b609eceb4ed46dcccd19328159b5fd74db87e
Chocolate-cereal <xue.shi.ucd.ie@gmail.com>
2026-09-18 08:26:53 +0100

    Refine shared public-site components

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,13 +2,7 @@ import { cn } from "@/lib/utils";
 
 export function Card({ className, children, ...props }: React.ComponentProps<"article">) {
   return (
-    <article
-      className={cn(
-        "rounded-2xl border border-border bg-surface p-6 shadow-soft transition hover:border-accent/45",
-        className,
-      )}
-      {...props}
-    >
+    <article className={cn("siteCard p-6", className)} {...props}>
       {children}
     </article>
   );
